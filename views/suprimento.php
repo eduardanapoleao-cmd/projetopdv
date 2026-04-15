4af937af94ae96f5c295087c51236b85aad4be44
eduardanapoleao-cmd <eduardanapoleao@alunos.utfpr.edu.br>
2026-04-15 11:55:22 -0300

    Feito CSS da pagina de suprimento

--- views/suprimento.php
+++ views/suprimento.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Suprimento</title>
+    <link rel="stylesheet" href="../css/suprimento.css"> </head>  
 </head>
 
 <body>
